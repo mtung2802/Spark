7e145e0b7f7a33804ff75098a8e0cfd1985df000
mtung2802 <118200925+mtung2802@users.noreply.github.com>
2025-03-10 00:14:05 +0700

    Update map.py

--- map.py
+++ map.py
@@ -4,7 +4,7 @@ from pyspark.shell import spark
 conf = SparkConf().setAppName("FileDataSource").setMaster("local[*]").set("spark.executor.memory", "2g")
 
 sc = spark.SparkContext(conf=conf)
-
+# map Không làm thay đổi số lượng phần tử của RDD, chỉ thay đổi giá trị của từng phần tử.
 fildeRdd = sc.textFile("../Data/DataDE.txt")
 
 allCapRdd = fildeRdd.map(lambda x: x.upper())
