94ff84aebd1276df6ff1fe6d6d1d6146042348c4
mtung2802 <118200925+mtung2802@users.noreply.github.com>
2025-04-16 14:34:26 +0700

    Delete SparkSQL directory

--- SparkSQL/CreateDF/CreateDFByReadingCsvFile.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-from pyspark.sql import Row
-from pyspark.sql.types import *
-from datetime import datetime
-spark = SparkSession.builder.appName("SparkSQL").master("local[*]").config("spark.executor.memory", "4g").getOrCreate()
-
-csvFile = spark.read.option("header", "value").option("inferSchema", "true").csv("C:\\Users\\Admin\\PycharmProjects\\DE-ETL-Spark\\Data\\DataDE.csv")
-csvFile.show(truncate=False)
