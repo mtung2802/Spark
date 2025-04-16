36aacc027ef627c9cee0ed47278e671d15289252
mtung2802 <118200925+mtung2802@users.noreply.github.com>
2025-04-16 14:34:12 +0700

    Delete Key-ValuePairRdd directory

--- Key-ValuePairRdd/Transformation/reduceBykey.py
+++
@@ -1,9 +0,0 @@
-from pyspark import SparkConf, SparkContext
-# nhom value cung key va cong voi nhau
-conf = SparkConf().setAppName("FileDataSource").setMaster("local[*]").set("spark.executor.memory", "2g")
-
-sc = SparkContext(conf=conf)
-
-data = sc.parallelize([("a", 1), ("a", 2), ("a", 3), ("b", 1), ("b", 2)])
-bill = data.reduceByKey(lambda x, y: x + y) # nhom value cung key va cong voi nhau
-print(bill.collect())
