a0591db64574d9716b51b9b5cc79f31b658770c9
mtung2802 <118200925+mtung2802@users.noreply.github.com>
2025-04-16 14:34:01 +0700

    Delete Actions directory

--- Actions/collect.py
+++
@@ -1,8 +0,0 @@
-# khi gọi collect thì đưa dữ liệu vào một list
-from pyspark import SparkConf, SparkContext
-
-conf = SparkConf().setAppName("FileDataSource").setMaster("local[*]").set("spark.executor.memory", "2g")
-
-sc = SparkContext(conf=conf)
-
-
